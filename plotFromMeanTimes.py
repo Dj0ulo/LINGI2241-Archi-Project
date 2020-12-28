9fb5cd60ac9a86395fe1e4c706074267f6a57bc1
gregoire <glechien@student.uclouvain.be>
2020-12-28 14:53:37 +0100

    for merge

--- plotFromMeanTimes.py
+++ plotFromMeanTimes.py
@@ -12,8 +12,8 @@ argv = sys.argv;
 # filename = "tests/rate-"+argv[1]+"-l="+argv[2]+"-maxres=1000.csv"
 
 
-simple = "tests/results-simple-l=3000-maxwords=5-maxres=10000.csv"
-opti = "tests/results-opti-l=3000-maxwords=5-maxres=10000.csv"
+simple = "tests/lambda-simple-1000.csv"
+opti = "tests/lambda-opti-1000.csv"
 
 
 def readData(filename):
@@ -41,7 +41,7 @@ waitssimple, timessimple, PoissonMeansimple = readData(simple)
 
 
 
-#print(len(timesopti), sum(timesopti)/len(timesopti))
+print(len(timesopti), sum(timesopti)/len(timesopti))
 plt.hist(timessimple, bins=25, density=1, facecolor='blue', alpha=0.5, label="simple")
 plt.hist(timesopti, bins=25, density=1, facecolor='green', alpha=0.5, label="opti")
 plt.axvline(np.mean(timessimple), -0.005, np.max(np.append(timesopti,timessimple)), label='simple mean ('+np.mean(timessimple).astype('str')+')', c='b', dashes = (5, 2, 1, 2) )
@@ -53,20 +53,5 @@ plt.ylabel("Proportions of requests")
 plt.xlabel("time [ms]")
 
 plt.title("Distribution of the response time of the requests")
- 
- 
-# x = []
-# for nb in nbIter:
-#     if nb not in x:
-#         x.append(nb)
-
-# y = []
-# for nb in x:
-#     timefornb = []
-#     for i in range(len(times)):
-#         if nbIter[i] == nb:
-#             timefornb.append(times[i])
-#     y.append(np.mean(timefornb))
-    
-# plt.plot(x, y, '-b')
 plt.show()
+
